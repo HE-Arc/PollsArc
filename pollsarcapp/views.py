7758da2ce42f78adaa73232d0f58f742761ab371
Vincent Moulin <vincent.moulin@he-arc.ch>
2020-02-19 17:09:50 +0100

    define signin function in views

--- pollsarcapp/views.py
+++ pollsarcapp/views.py
@@ -33,4 +33,22 @@ def signup(request):
         form = UserCreationForm()
     return render(request, 'users/signup.html', {'form': form})
 
+def connexion(request):
+    error = False
+
+    if request.method == "POST":
+        form = ConnexionForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+            user = authenticate(username=username, password=password)
+            if user: 
+                login(request, user)
+            else: 
+                error = True
+    else:
+        form = ConnexionForm()
+
+    return render(request, 'users/connexion.html', locals())
+
 
