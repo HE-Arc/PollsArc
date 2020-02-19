08a50b984e5c376436bcbb763434cb1db672f053
Vincent Moulin <vincent.moulin@he-arc.ch>
2020-02-19 16:53:19 +0100

    Added path to signup and the view

--- pollsarcapp/views.py
+++ pollsarcapp/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.http import JsonResponse
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def home(request):
@@ -16,5 +18,19 @@ def searchUsers(request, name):
     for user in users:
         list_users.append((user.id, user.username))
     return JsonResponse({'users' : list_users})
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/signup.html', {'form': form})
 
 
