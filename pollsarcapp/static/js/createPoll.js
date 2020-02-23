297976bede0f1722409fb2e5ce7a6b8d6a2639df
Romain Capocasale <romain.capocasale@he-arc.ch>
2020-02-23 17:38:11 +0100

    add delete proposed proposition on poll form

--- pollsarcapp/static/js/createPoll.js
+++ pollsarcapp/static/js/createPoll.js
@@ -16,7 +16,7 @@ document.getElementById("search_user").addEventListener("input", (e) => {
                         if(!user_already_selected(added_users, added_user)){
                             added_users.push([ui.item.id, added_user])
                             
-                            document.getElementById("selected_user_display").innerHTML += "<div class='user' id="+ ui.item.id +">"+ added_user +'<button type="button" class="close" onclick="removeUser(this)" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>'
+                            document.getElementById("selected_user_display").innerHTML += "<div class='user' id=user_"+ ui.item.id +">"+ added_user +'<button type="button" class="close" onclick="removeUser(this)" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>'
     
                             document.getElementById("selected_user").value = JSON.stringify(added_users.map((v,i) => {return v[0]}))
                         }                    
@@ -30,31 +30,50 @@ function user_already_selected(users_array, user){
     return users_array.map((v,i) => {return v[1]}).includes(user)
 }
 
-let nb_prop = 0
-let proposed_props = []
-
-document.getElementById("add_proposition_button").addEventListener("click", (e) => {
-    var prop = document.getElementById("add_proposition").value
-    nb_prop++
-    proposed_props.push(prop)
-
-    document.getElementById("proposed_prop_display").innerHTML += "<p><strong>"+ nb_prop + ")</strong> "+ prop +"</p>"
-
-    document.getElementById("proposed_prop").value = JSON.stringify(proposed_props)
-})
-
 function removeUser(element){
     let parent = element.parentNode
 
     let index_to_remove = 0;
     added_users.forEach( (elem, index) =>{
-        if(elem == parent.id){
+        if("user_" + elem === parent.id){
             index_to_remove = index
         }
     })
     added_users.splice(index_to_remove,1)
     parent.remove()
 }
+
+let nb_prop = 0
+let proposed_props = []
+
+document.getElementById("add_proposition_button").addEventListener("click", (e) => {
+    let prop_input = document.getElementById("add_proposition")
+    let prop = prop_input.value
+
+    prop_input.value = ""
+    prop_input.focus()
+
+    nb_prop++
+    proposed_props.push(prop)
+
+    //document.getElementById("proposed_prop_display").innerHTML += "<span><strong>"+ nb_prop + ")</strong> "+ prop +"</span>"
+    document.getElementById("proposed_prop_display").innerHTML += "<div id='prop_'" + nb_prop + "><strong>"+ nb_prop + ")</strong> "+ prop +'<button type="button" class="close" onclick="removeProp(this)" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>'
+    document.getElementById("proposed_prop").value = JSON.stringify(proposed_props)
+})
+
+function removeProp(element){
+    let parent = element.parentNode
+
+    let index_to_remove = 0;
+    proposed_props.forEach( (elem, index) =>{
+        if("prop_" + elem === parent.id){
+            index_to_remove = index
+        }
+    })
+    proposed_props.splice(index_to_remove,1)
+    parent.remove()
+}
+
 
 // Allow to don't send the search field on the server
 $("#poll_form").submit(() => {
