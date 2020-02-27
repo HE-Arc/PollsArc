b998d233e0fc8541ea08d6e163c1ca627787729f
Vincent Moulin <vincent.moulin@he-arc.ch>
2020-02-27 13:21:55 +0100

    password reset and login with email works

--- pollsarcapp/views.py
+++ pollsarcapp/views.py
@@ -71,20 +71,6 @@ def addUsersToPoll(id_users, poll):
         return True
     except User.DoesNotExist:
         return False
-      
-def register_old(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user=authenticate(username=username, password=raw_password)
-            login(request, user)
-            return redirect('home')
-    else:
-        form = UserCreationForm()
-    return render(request, 'registration/register.html', {'form': form})
 
 def register(request):
     if request.method == 'POST':
