425ca4933acadaf5a812ad3f9f6fd24ca518419c
Romain Capocasale <romain.capocasale@he-arc.ch>
2020-04-01 13:02:09 +0200

    add comment in js file

--- pollsarcapp/static/js/createPoll.js
+++ pollsarcapp/static/js/createPoll.js
@@ -1,12 +1,15 @@
-let added_users = []
+let added_users = [] //contains the invited user
 
 let search_user_input = document.getElementById("search_user")
 
 if(search_user_input){
+    // Makes a GET query to get users from the user search
     search_user_input.addEventListener("input", (e) => {
         fetch('searchUsers/' + encodeURI(e.data)).then((response) => {
             response.json().then((data) => {
-                user_to_display = [];
+
+                // This array contains users from the GET research
+                user_to_display = []; 
                 for(let i = 0; i < data.users.length; i++){
                     user_to_display.push(data.users[i].pseudo)
                 }
@@ -16,6 +19,8 @@ if(search_user_input){
                     select: function(e, ui){
     
                         let added_user = ui.item.label
+
+                            // if the user isn't already in the invited list, add the user to the invited user array and display on the page
                             if(!user_already_selected(added_users, added_user)){
                                 added_users.push([ui.item.id, added_user])
                                 
@@ -29,8 +34,9 @@ if(search_user_input){
     })
 }
 
-let is_private_checkbox = document.getElementById('is_private')
+let is_private_checkbox = document.getElementById('is_private') 
 
+// Allow to remove the user search field if the private checkbox isn't checked, othwersiw display the search field
 if(is_private_checkbox)
 {
     is_private_checkbox.addEventListener('change', function(e) {
@@ -45,10 +51,12 @@ if(is_private_checkbox)
     })
 }
 
+//Return a boolean tha indicated if the user is already present in the invited user list
 function user_already_selected(users_array, user){
     return users_array.map((v,i) => {return v[1]}).includes(user)
 }
 
+// remove a user from the page and the array
 function removeUser(element){
     let parent = element.parentNode
 
@@ -62,21 +70,24 @@ function removeUser(element){
     parent.remove()
 }
 
-let nb_prop = 0
-let proposed_props = []
+let nb_prop = 0 // index of the current proposition
+let proposed_props = [] // array that contain the proposition
 
-let prop_button = document.getElementById("add_proposition_button")
+let prop_button = document.getElementById("add_proposition_button") // proposition button
 
+// if the proposition exist, add a proposition
 if(prop_button){
     prop_button.addEventListener("click", (e) => {
         addProposition();
     })
 }
 
+// Add a proposition on the page and on the propositon array
 function addProposition(){
     let prop_input = document.getElementById("add_proposition")
     let prop = prop_input.value
 
+    //if the proposition begin by a number, interupt the function
     if (prop == "" || prop.match(/^\d/))
         return;
 
@@ -90,6 +101,7 @@ function addProposition(){
     document.getElementById("proposed_prop").value = JSON.stringify(proposed_props)
 }
 
+// Remove a added propositon on the page and from the array
 function removeProp(element){
     let parent = element.parentNode
 
@@ -100,15 +112,16 @@ function removeProp(element){
 }
 
 
-// Allow to don't send the search field on the server
+//Check if minimum a proposition is filled
 $("#poll_form").submit(() => {
-    /*if(proposed_props.length == 0){
+    if(proposed_props.length == 0){
         alert("You must enter propositions ! ")
         return false
     }else {
+        // Allow to don't send the search field on the server
         $("#search_user").prop('disabled', true)
         return true
-    }*/$("#search_user").prop('disabled', true)
+    }
     return true
 })
 
