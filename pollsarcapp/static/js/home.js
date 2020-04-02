c6dd4494f14313df0750fe434124a7c46b7baea5
Alexandre Bianchi <alexandre.bianchi@nectoria.com>
2020-04-02 17:50:26 +0200

    Fix missing semicolon

--- pollsarcapp/static/js/home.js
+++ pollsarcapp/static/js/home.js
@@ -1,18 +1,18 @@
 
-let searchPollInput = document.getElementById("search-poll")
+let searchPollInput = document.getElementById("search-poll");
 
 if (searchPollInput) {
-    let proposedPollsInput = document.getElementById("proposed-polls")
+    let proposedPollsInput = document.getElementById("proposed-polls");
     // Makes a GET query to get polls from the user research
     searchPollInput.addEventListener("input", (e) => {
         fetch('search_polls/' + encodeURI(e.data)).then((response) => {
-            proposedPollsInput.innerHTML = ""
+            proposedPollsInput.innerHTML = "";
             response.json().then((data) => {
                 data.polls.forEach(element => {
                     //display the poll on the page
-                    proposedPollsInput.innerHTML += "<div class='proposed-poll-display'><a href='poll/" + element.id + "'><strong>Name : " + element.name + "</strong><br> descrption : " + element.description + "</a></div>"
+                    proposedPollsInput.innerHTML += "<div class='proposed-poll-display'><a href='poll/" + element.id + "'><strong>Name : " + element.name + "</strong><br> descrption : " + element.description + "</a></div>";
                 });
-            })
-        })
-    })
+            });
+        });
+    });
 }
