a5e3f1963562b289cf2332784e2665813a7728e4
Alexandre Bianchi <alexandre.bianchi@nectoria.com>
2020-04-03 22:06:02 +0200

    Fix directory

--- pollsarcproject/settings/development.py
+++ pollsarcproject/settings/development.py
@@ -14,7 +14,7 @@ import os
 from django.contrib.messages import constants as message_constants
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
@@ -62,7 +62,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, '../templates'),
+            os.path.join(BASE_DIR, 'templates'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
