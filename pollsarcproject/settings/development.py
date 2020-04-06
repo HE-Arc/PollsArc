15c943fd5a2c72fc582264cdcc59298a9217d778
Alexandre Bianchi <alexandre.bianchi@nectoria.com>
2020-04-06 22:21:23 +0200

    Fix path

--- pollsarcproject/settings/development.py
+++ pollsarcproject/settings/development.py
@@ -14,7 +14,7 @@ import os
 from django.contrib.messages import constants as message_constants
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -125,15 +125,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = 'd:/TEST/static/'
+#STATIC_ROOT = 'd:/TEST/static/'
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, "static"),
+#)
 
-MEDIA_ROOT = 'd:/TEST/media/'
-MEDIA_URL = '/media/'
+#MEDIA_ROOT = 'd:/TEST/media/'
+#MEDIA_URL = '/media/'
 
 # Redirect to home URL after login (Default redirects to /accounts/profile/)
 LOGIN_REDIRECT_URL = '/'
